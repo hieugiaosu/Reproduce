c8f3cf4fdaa585b8db778c43dcd39c15926689c2
hieugiaosu <phamthehieuvtt123@gmail.com>
2024-07-09 11:38:40 +0700

    change loss display

--- training/pipeline/SepReformerBasePipeline.py
+++ training/pipeline/SepReformerBasePipeline.py
@@ -120,10 +120,10 @@ class SepReformerBasePipeLine(TrainPipeline):
             cur_loss.backward()
             torch.nn.utils.clip_grad_norm_(self.model.parameters(),5.0)
             self.optimizer.step()
-            print()
             dict_loss = {"T_Loss": tot_loss_time / num_batch}
             dict_loss.update({'F_Loss_' + str(idx): loss / num_batch for idx, loss in enumerate(tot_loss_freq)})
             pbar.set_postfix(dict_loss)
+            print(" ".join([f"{key}:{val}" for key, val in dict_loss.items()]))
             del cur_loss_s_bn, mixture, src, cur_loss_s, alpha, cur_loss, estim_src, estim_src_bn, dict_loss
             torch.cuda.empty_cache()
             gc.collect()
